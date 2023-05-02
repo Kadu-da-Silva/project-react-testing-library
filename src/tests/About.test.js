55319418a64c150e104bdc90f5953d10666c1778
Carlos Eduardo <carlao0010@hotmail.com>
2023-05-02 16:16:25 -0300

    Update About.test.js

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,29 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import About from '../pages/About';
+
+describe('Testa o componente About', () => {
+  it('Teste se a página contém as informações sobre a Pokédex', () => {
+    renderWithRouter(<About />);
+
+    const textInfo = screen.getByText('This application simulates a Pokédex, a digital encyclopedia containing all Pokémon');
+    expect(textInfo).toBeDefined();
+  });
+
+  it('Teste se a página contém um heading h2 com o texto About Pokédex', () => {
+    renderWithRouter(<About />);
+
+    const headingEl = screen.getByRole('heading', { name: /About Pokédex/i, level: 2 });
+    expect(headingEl).toBeInTheDocument();
+  });
+
+  it('Teste se a página contém a seguinte imagem de uma Pokédex: https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png', () => {
+    renderWithRouter(<About />);
+
+    const urlImg = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+
+    const img = screen.getByRole('img');
+    expect(img).toHaveAttribute('src', urlImg);
+  });
+});
