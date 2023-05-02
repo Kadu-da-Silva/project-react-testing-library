4ab49341ac82bf8076180ae7dee01808a595b31f
Carlos Eduardo <carlao0010@hotmail.com>
2023-05-02 18:33:38 -0300

    Update NotFound.test.js

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../pages/NotFound';
+
+describe('Teste o componente NotFound', () => {
+  it('Teste se a página contém um heading h2 com o texto Page requested not found', () => {
+    renderWithRouter(<NotFound />);
+
+    const headingEl = screen.getByRole('heading', { name: 'Page requested not found', level: 2 });
+    expect(headingEl).toBeDefined();
+  });
+
+  it('Teste se a página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+    renderWithRouter(<NotFound />);
+
+    const img = screen.getByRole('img');
+    expect(img).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
