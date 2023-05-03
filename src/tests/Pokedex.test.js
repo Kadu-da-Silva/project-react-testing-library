a6b4e8c8105a78c880533ce19e46d35cd1c0391a
Carlos Eduardo <carlao0010@hotmail.com>
2023-05-02 21:40:10 -0300

    Update Pokedex.test.js

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -3,21 +3,40 @@ import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 // import { act } from 'react-dom/test-utils';
 import renderWithRouter from '../renderWithRouter';
-import App from '../App';
+import { Pokedex } from '../pages';
+import pokemonList from '../data';
 
 const pokemonNameId = 'pokemon-name'; // Para o Lint parar de encher o saco.
 const pokemonTypeId = 'pokemon-type'; // Para o Lint parar de encher o saco.
 
+const favoritePokemon = []; // ou favoritePokemon = pokemonList para todos os pokemons favoritos
+const isPokemonFavoriteById = {}; // ou isPokemonFavoriteById = {1: false, 2: true, ...} para todos os pokemons
+const onUpdateFavoritePokemon = jest.fn(); // ou outra implementação para testar a função
+
 describe('Testa o componente Pokedex', () => {
   it('Teste se a página contém um heading h2 com o texto Encountered Pokémon', () => {
-    renderWithRouter(<App />);
+    renderWithRouter(
+      <Pokedex
+        favoritePokemon={ favoritePokemon }
+        pokemonList={ pokemonList }
+        isPokemonFavoriteById={ isPokemonFavoriteById }
+        onUpdateFavoritePokemon={ onUpdateFavoritePokemon }
+      />,
+    );
 
     const headingEl = screen.getByRole('heading', { name: /Encountered Pokémon/i, level: 2 });
     expect(headingEl).toBeDefined();
   });
 
   it('Teste se é exibido o próximo Pokémon da lista quando o botão Próximo Pokémon é clicado', async () => {
-    renderWithRouter(<App />);
+    renderWithRouter(
+      <Pokedex
+        favoritePokemon={ favoritePokemon }
+        pokemonList={ pokemonList }
+        isPokemonFavoriteById={ isPokemonFavoriteById }
+        onUpdateFavoritePokemon={ onUpdateFavoritePokemon }
+      />,
+    );
 
     const button = screen.getByRole('button', { name: /Próximo Pokémon/i });
     expect(button).toBeDefined();
@@ -38,8 +57,14 @@ describe('Testa o componente Pokedex', () => {
   });
 
   it('O primeiro Pokémon da lista deve ser mostrado ao clicar no botão, se estiver no último Pokémon da lista.', () => {
-    renderWithRouter(<App />);
-
+    renderWithRouter(
+      <Pokedex
+        favoritePokemon={ favoritePokemon }
+        pokemonList={ pokemonList }
+        isPokemonFavoriteById={ isPokemonFavoriteById }
+        onUpdateFavoritePokemon={ onUpdateFavoritePokemon }
+      />,
+    );
     let clickCount = 0;
     let nextButton = screen.getByRole('button', { name: /Próximo Pokémon/i });
 
@@ -53,7 +78,14 @@ describe('Testa o componente Pokedex', () => {
   });
 
   it('Teste se é mostrado apenas um Pokémon por vez', () => {
-    renderWithRouter(<App />);
+    renderWithRouter(
+      <Pokedex
+        favoritePokemon={ favoritePokemon }
+        pokemonList={ pokemonList }
+        isPokemonFavoriteById={ isPokemonFavoriteById }
+        onUpdateFavoritePokemon={ onUpdateFavoritePokemon }
+      />,
+    );
 
     expect(screen.getByTestId(pokemonNameId)).toHaveTextContent('Pikachu');
 
@@ -64,7 +96,14 @@ describe('Testa o componente Pokedex', () => {
   });
 
   it('Deve existir um botão de filtragem para cada tipo de Pokémon, sem repetição', () => {
-    renderWithRouter(<App />);
+    renderWithRouter(
+      <Pokedex
+        favoritePokemon={ favoritePokemon }
+        pokemonList={ pokemonList }
+        isPokemonFavoriteById={ isPokemonFavoriteById }
+        onUpdateFavoritePokemon={ onUpdateFavoritePokemon }
+      />,
+    );
 
     const buttons = screen.getAllByTestId('pokemon-type-button');
 
@@ -74,7 +113,14 @@ describe('Testa o componente Pokedex', () => {
   });
 
   it('A partir da seleção de um botão de tipo, a Pokédex deve circular somente pelos Pokémon daquele tipo', () => {
-    renderWithRouter(<App />);
+    renderWithRouter(
+      <Pokedex
+        favoritePokemon={ favoritePokemon }
+        pokemonList={ pokemonList }
+        isPokemonFavoriteById={ isPokemonFavoriteById }
+        onUpdateFavoritePokemon={ onUpdateFavoritePokemon }
+      />,
+    );
 
     const buttonType = screen.getByRole('button', { name: 'Fire' });
     userEvent.click(buttonType);
@@ -88,7 +134,14 @@ describe('Testa o componente Pokedex', () => {
   });
 
   it('O texto do botão deve corresponder ao nome do tipo, ex. Psychic', () => {
-    renderWithRouter(<App />);
+    renderWithRouter(
+      <Pokedex
+        favoritePokemon={ favoritePokemon }
+        pokemonList={ pokemonList }
+        isPokemonFavoriteById={ isPokemonFavoriteById }
+        onUpdateFavoritePokemon={ onUpdateFavoritePokemon }
+      />,
+    );
 
     const buttonType = screen.getByRole('button', { name: 'Psychic' });
     userEvent.click(buttonType);
@@ -99,7 +152,14 @@ describe('Testa o componente Pokedex', () => {
   });
 
   it('O botão All precisa estar sempre visível', () => {
-    renderWithRouter(<App />);
+    renderWithRouter(
+      <Pokedex
+        favoritePokemon={ favoritePokemon }
+        pokemonList={ pokemonList }
+        isPokemonFavoriteById={ isPokemonFavoriteById }
+        onUpdateFavoritePokemon={ onUpdateFavoritePokemon }
+      />,
+    );
 
     const buttonType = screen.getByRole('button', { name: 'Psychic' });
     userEvent.click(buttonType);
