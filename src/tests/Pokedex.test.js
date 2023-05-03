36f041ad92d74594b18238602194ce700338112d
Carlos Eduardo <carlao0010@hotmail.com>
2023-05-03 08:36:30 -0300

    Update Pokedex.test.js

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-// import { act } from 'react-dom/test-utils';
 import renderWithRouter from '../renderWithRouter';
 import pokemonList from '../data';
 import App from '../App';
